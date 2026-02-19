29ed75e86082597f378e5074ad437b4a308e6e58
XolaniG <34442183+XolaniGqamana@users.noreply.github.com>
2026-02-19 09:27:32 +0200

    refactor nohelper.py
    added a helper function

--- nohelper.py
+++ nohelper.py
@@ -1,5 +1,8 @@
-def processNumber(a):
-  if a % 2 == 0:
+def isEven(a): # this is the helper function
+  return a % 2 == 0
+
+def processNumber(b):
+  if isEven(b):
     print("even number")
   else:
     print("odd number")
